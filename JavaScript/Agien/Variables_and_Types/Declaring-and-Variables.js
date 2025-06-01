61eba1d24e23ec84857e2325a14de0359b6a4041
prakash-it <prakashkandasamy4@gmail.com>
2025-06-01 21:50:56 +0530

    String

--- JavaScript/Agien/Variables_and_Types/Declaring-and-Variables.js
+++ JavaScript/Agien/Variables_and_Types/Declaring-and-Variables.js
@@ -1,2 +1,19 @@
-var x =32;
-x
+var a=15;
+let b= 16;
+console.log(a)
+// Arthamatic operator +, -, *, **, /, %, ++, --
+console.log(a+b)
+console.log(a-b)
+
+console.log(x/y)
+
+console.log(a*b)
+console.log(a%b)
+
+console.log(a**2)
+
+console.log(a+a)
+
+console.log(++a)
+
+console.log(--b)
---
+++ JavaScript/Agien/Variables_and_Types/String and properties.js
@@ -0,0 +1,9 @@
+const x= "Like";
+const y= "hate";
+//string has be declear 
+console.log(x , y)
+//string have be direct declear of console
+console.log("This string is used to doblequtos")
+
+let str = "hi this a string learn"
+console.log(str.toUpperCase())
