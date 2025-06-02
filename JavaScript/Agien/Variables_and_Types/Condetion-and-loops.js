bc6c6afbc028bb6d99e7e6eedd9c8f81fd0eebd5
prakash-it <prakashkandasamy4@gmail.com>
2025-06-02 23:26:08 +0530

    Conditon

--- JavaScript/Agien/Variables_and_Types/Condetion-and-loops.js
+++ JavaScript/Agien/Variables_and_Types/Condetion-and-loops.js
@@ -20,7 +20,7 @@ if(a<=50 && b<=80)
 
 }
 
-    else if(a==65 && b>=a)
+    else if(a!=65 && b>=a)
     {
         console.log("You are already here");
         
@@ -30,3 +30,15 @@ if(a<=50 && b<=80)
         console.log("you are fire");
         
     }
+
+
+
+    let we = 100
+    let are = 200
+
+    let max
+
+    if(we>are) max=we;
+    else max=are
+    console.log(max);
+    
