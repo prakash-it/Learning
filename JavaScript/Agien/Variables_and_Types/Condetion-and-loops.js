85621aac5a7886b8f9ce9e92b739c1d9de2bf151
prakash-it <prakashkandasamy4@gmail.com>
2025-06-02 23:23:19 +0530

    Conditon

---
+++ JavaScript/Agien/Variables_and_Types/Condetion-and-loops.js
@@ -0,0 +1,32 @@
+let pass =true
+
+let fall = false
+
+if(pass!=fall){
+  console.log("This condition is right ");
+  
+}
+else{
+console.log("This condition is not right");
+
+}
+
+let a= 65;
+let b=75;
+
+if(a<=50 && b<=80)
+{
+    console.log("Welocme to the underworld");
+
+}
+
+    else if(a==65 && b>=a)
+    {
+        console.log("You are already here");
+        
+    }
+
+    else{
+        console.log("you are fire");
+        
+    }
